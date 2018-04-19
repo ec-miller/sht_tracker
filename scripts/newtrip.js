f3eda433d0c3eccf77071fb216e8fa8b4a4feea3
cynthia clinton <cynthiaclinton@cynthias-MacBook-Pro.local>
2018-04-19 00:04:28 -0400

    Create Form, Add Background Image, Add CSS and JS To newTrip

---
+++ scripts/newtrip.js
@@ -0,0 +1,14 @@
+let counter = 1;
+let limit = 5;
+function addInput(divName){
+
+     if (counter == limit)  {
+          alert("You have reached the limit of adding " + counter + " emails");
+     }
+     else {
+          let newdiv = document.createElement('div');
+          newdiv.innerHTML = "Email " + (counter + 1) + ' <br><input type="text"  placeholder="Email" class="myEmail[]">';
+          document.getElementById(divName).appendChild(newdiv);
+          counter++;
+     }
+}
