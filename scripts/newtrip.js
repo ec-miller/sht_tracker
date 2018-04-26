e36efd798f4f0ed45576dd671d1f5da563d3c684
Eric <ericmiller@umn.edu>
2018-04-26 09:15:06 -0500

    Edited form CSS to remove blue highlight in inputs, changed Add E-mail to button, added autocomplete for emails

--- scripts/newtrip.js
+++ scripts/newtrip.js
@@ -1,13 +1,14 @@
 let counter = 1;
 let limit = 5;
 function addInput(divName){
+  event.preventDefault();
 
      if (counter == limit)  {
           alert("You have reached the limit of adding " + counter + " emails");
      }
      else {
           let newdiv = document.createElement('div');
-          newdiv.innerHTML = "Email " + (counter + 1) + ' <br><input type="text"  placeholder="Email" class="form-control">';
+          newdiv.innerHTML = "Email " + (counter + 1) + ' <br><input type="email" autocomplete="email" placeholder="Email" class="form-control">';
           document.getElementById(divName).appendChild(newdiv);
           counter++;
      }
